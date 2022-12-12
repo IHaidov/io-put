1141a1bf245c86a3f6b0bfe720ea705a1868f5ea
IHaidov <38441739+IHaidov@users.noreply.github.com>
2022-12-13 00:06:46 +0100

    Update TextTransformerController.java

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,46 +1,59 @@
 package pl.put.poznan.transformer.rest;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.TextTransformer;
+import pl.put.poznan.transformer.json_format.transInfo;
+import pl.put.poznan.transformer.logic.*;
 
 import java.util.Arrays;
 
 
 @RestController
-@RequestMapping("/{text}")
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value ="/TextTransform/{text}", method = RequestMethod.GET, produces = "application/json")
     public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+                      @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
         // log the parameters
         logger.debug(text);
         logger.debug(Arrays.toString(transforms));
 
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+        return listTrans(transforms).transform(text);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
+    @RequestMapping(value = "/TextTransform", method = RequestMethod.GET, produces = "application/json")
+    public String post(@RequestBody transInfo info) throws JSONException {
 
         // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        logger.debug(info.getText());
+        logger.debug(Arrays.toString(info.getTransforms()));
 
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+        JSONObject response = new JSONObject();
+        response.put("text", listTrans(info.getTransforms()).transform(info.getText()));
+
+        return response.toString();
     }
 
+    private static IText listTrans(String[] transforms) {
+        IText transformation = new InputString();
+        for (String trans : transforms) transformation = chooseTransformation(trans, transformation);
+        return transformation;
+    }
 
-
+    private static IText chooseTransformation(String name, IText transI) {
+        return switch (name) {
+            case "upper" -> new ToUpper(transI);
+            case "reverse" -> new Reverse(transI);
+            case "lower" -> new ToLower(transI);
+            case "capitalize" -> new Capitalize(transI);
+            default -> transI;
+        };
+    }
 }
 
 
